4d87e64ffd484a0139f5916348bc11c7104cd632
marvim1990 <marcosvinim.cruz@gmail.com>
2023-05-07 20:00:20 -0300

    Global css e header

---
+++ src/Post.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+
+export function Post(props){
+  return(
+    <div>
+      <strong>{props.author}</strong>
+      <p>{props.content}</p>
+    </div>
+  ) 
+}
